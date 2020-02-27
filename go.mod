0bbcd2a29b94a4351b5c120737d8b5709eb8bbba
Senny Aaron <arumeru@gmail.com>
2020-02-27 15:12:14 +0300

    Organized Code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/senny-matrix/go-architecture
+
+go 1.13
---
+++ storage/mongo/mongo.go
@@ -0,0 +1,13 @@
+package mongo
+
+import "github.com/senny-matrix/go-architecture"
+
+type Db map[int]architecture.Person
+
+func (m Db) Save(n int, p architecture.Person) {
+	m[n] = p
+}
+
+func (m Db) Retrieve(n int) architecture.Person {
+	return m[n]
+}
